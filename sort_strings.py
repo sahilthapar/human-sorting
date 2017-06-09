a176eaf71e681b417d400d4f7949cb9f04ec5394
Sahil Thapar <sahilthapar1992@gmail.com>
2017-06-09 18:21:59 -0400

    Added support for dates and money

--- sort_strings.py
+++ sort_strings.py
@@ -1,4 +1,28 @@
 import re
+import datetime
+
+num_re = re.compile(r"[+-.]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?")
+money_re = re.compile(r"^\$\d+[.]?\d+$")
+date_re = re.compile(r"^\d{4}[-/]\d{2}[-/]\d{2}$")
+
+def regex_chooser(str_a):
+
+  ret_a = {'value': str_a}
+
+  if date_re.match(str_a):
+    ret_a['type'] = 'date'
+    ret_a['comp_value'] = (datetime.datetime.strptime(str_a, '%Y-%m-%d') - datetime.datetime.utcfromtimestamp(0)).total_seconds()
+
+  elif money_re.match(str_a):
+    ret_a['type'] = 'money'
+    ret_a['comp_value'] = float(str_a.replace('$', ''))
+
+  elif num_re.match(str_a):
+    ret_a['type'] = 'number'
+    ret_a['comp_value'] = float(str_a)
+
+  return ret_a
+
 
 def compare_strings(str_a, str_b):
   print str_a
@@ -10,11 +34,12 @@ def compare_strings(str_a, str_b):
   GREATER = 1
 
   # Check if the strings are numbers
-  num_re = r"[+-.]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?"
-  chk_a = float(nrm_a) if re.compile(num_re).match(nrm_a) else nrm_a
+  chk_a = float(nrm_a) if num_re.match(nrm_a) else nrm_a
   chk_b = float(nrm_b) if re.compile(num_re).match(nrm_b) else nrm_b
   return EQUAL if chk_a == chk_b else (SMALLER if chk_a < chk_b else GREATER)
 
 
 def sortStrings(strings):
-  return sorted(strings, cmp=compare_strings)
+  typed_strings = [regex_chooser(i) for i in strings]
+  sorted_strings = sorted(typed_strings, key=lambda x: (['type'], x['comp_value']))
+  return [x['value'] for x in sorted_strings]
--- sort_strings_spec.py
+++ sort_strings_spec.py
@@ -13,6 +13,16 @@ class TestSortStrings(unittest.TestCase):
     sorted_numbers = ['-2.23E+45', '-21', '-13', '0.1e-456', '.2', '1', '+1', '2', '2e99', '11e99']
     self.assertEqual(sort_strings.sortStrings(numbers), sorted_numbers)
 
+  def test_money(self):
+    money = ['$5000', '$4000.10', '$400.10', '$0.99']
+    sorted_money = ['$0.99', '$400.10', '$4000.10', '$5000']
+    self.assertEqual(sort_strings.sortStrings(money), sorted_money)
+
+  def test_date(self):
+    dates = ['2016-08-01', '2017-01-01', '2010-01-08', '1992-05-06']
+    sorted_dates = ['1992-05-06', '2010-01-08', '2016-08-01', '2017-01-01']
+    self.assertEqual(sort_strings.sortStrings(dates), sorted_dates)
+
 if __name__ == '__main__':
   unittest.main()
 
