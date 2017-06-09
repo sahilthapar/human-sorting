b8c80713f841cc562cbc4b42e3b3a337f780d48d
Sahil Thapar <sahilthapar1992@gmail.com>
2017-06-09 16:10:29 -0400

    Added support for numbers in scientific notation

--- sort_strings.py
+++ sort_strings.py
@@ -1,6 +1,7 @@
 import re
 
 def compare_strings(str_a, str_b):
+  print str_a
   # Normalized strings
   nrm_a = str_a.lower()
   nrm_b = str_b.lower()
@@ -9,7 +10,7 @@ def compare_strings(str_a, str_b):
   GREATER = 1
 
   # Check if the strings are numbers
-  num_re = r'[+-.]?\d+(?:\.\d+)?'
+  num_re = r"[+-.]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?"
   chk_a = float(nrm_a) if re.compile(num_re).match(nrm_a) else nrm_a
   chk_b = float(nrm_b) if re.compile(num_re).match(nrm_b) else nrm_b
   return EQUAL if chk_a == chk_b else (SMALLER if chk_a < chk_b else GREATER)
--- sort_strings_spec.py
+++ sort_strings_spec.py
@@ -8,6 +8,11 @@ class TestSortStrings(unittest.TestCase):
     sorted_numbers = ['-21', '-13', '-1.1', '.2', '1', '+1', '2', '13']
     self.assertEqual(sort_strings.sortStrings(numbers), sorted_numbers)
 
+  def test_scientific_notation_numbers(self):
+    numbers = ['1', '11e99', '2e99' ,'2', '-2.23E+45', '-13', '0.1e-456', '+1', '.2', '-21']
+    sorted_numbers = ['-2.23E+45', '-21', '-13', '0.1e-456', '.2', '1', '+1', '2', '2e99', '11e99']
+    self.assertEqual(sort_strings.sortStrings(numbers), sorted_numbers)
+
 if __name__ == '__main__':
   unittest.main()
 
