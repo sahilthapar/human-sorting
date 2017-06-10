58714e0d151809ec25e1cace3ba4bae590c7a7b7
Sahil Thapar <sahilthapar1992@gmail.com>
2017-06-09 23:30:52 -0400

    Whole bunch of refactoring

--- sort_strings.py
+++ sort_strings.py
@@ -1,58 +1,15 @@
-import re
 import datetime
+from utils import *
 
-# Most of the regex have been sourced from stackoverflow
-
-num_re = re.compile(r"^[+-.]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?$")
-money_re = re.compile(r"^\$\d+[.]?\d+$")
-date_re = re.compile(r"^\d{4}[-/]\d{2}[-/]\d{2}$")
-phone_number_re = re.compile(r'^(\d{3})-(\d{3})-(\d{4})$')
-ip_address_re = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")
-file_path_re = re.compile(r"^[.]?(/[^/ ]*)+/?$")
-version_re = re.compile(r"(\d+\.)?(\d+\.)?(\*|\d+)")
-
-def regex_chooser(str_a):
-
-  ret_a = {'value': str_a}
-  if file_path_re.match(str_a):
-    ret_a['type'] = 'file_path'
-    file_parts = str_a.split('/')
-    ret_a['comp_value'] = (len(file_parts), ) + tuple(file_parts)
-
-  elif date_re.match(str_a):
-    ret_a['type'] = 'date'
-    ret_a['comp_value'] = (datetime.datetime.strptime(str_a, '%Y-%m-%d') -
-                           datetime.datetime.utcfromtimestamp(0)).total_seconds()
-
-  elif phone_number_re.match(str_a):
-    ret_a['type'] = 'phone_number'
-    ret_a['comp_value'] = phone_number_re.match(str_a).groups()
-
-
-  elif ip_address_re.match(str_a):
-    ret_a['type'] = 'ip_address'
-    ret_a['comp_value'] = tuple([int(x) for x in str_a.split('.')])
-
-  elif money_re.match(str_a):
-    ret_a['type'] = 'money'
-    ret_a['comp_value'] = float(str_a.replace('$', ''))
-
-  elif num_re.match(str_a):
-    ret_a['type'] = 'number'
-    ret_a['comp_value'] = float(str_a)
-
-  elif version_re.match(str_a):
-    ret_a['type'] = 'version'
-    ret_a['comp_value'] = tuple([int(x) for x in str_a.split('.')])
-
-  else:
-    ret_a['type'] = 'alphanum'
-    convert = lambda s: float(s) if num_re.match(s) else s
-    ret_a['comp_value'] = tuple([convert(c) for c in re.split('([0-9]+)', str_a.lower())])
-
-  return ret_a
+def add_type_compare_value(str):
+  type = get_type(str)
+  return {
+    'value': str,
+    'type': type,
+    'comp_value': get_compare_value_getter(type)(str)
+  }
 
 def sortStrings(strings):
-  typed_strings = [regex_chooser(i) for i in strings]
+  typed_strings = [add_type_compare_value(i) for i in strings]
   sorted_strings = sorted(typed_strings, key=lambda x: (x['type'], x['comp_value']))
   return [x['value'] for x in sorted_strings]
---
+++ utils.py
@@ -0,0 +1,76 @@
+import datetime
+import re
+
+num_re = re.compile(r"^[+-.]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?$")
+money_re = re.compile(r"^\$\d+[.]?\d+$")
+date_re = re.compile(r"^\d{4}[-/]\d{2}[-/]\d{2}$")
+phone_number_re = re.compile(r'^(\d{3})-(\d{3})-(\d{4})$')
+ip_address_re = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")
+file_path_re = re.compile(r"^[.]?(/[^/ ]*)+/?$")
+version_re = re.compile(r"(\d+\.)?(\d+\.)?(\*|\d+)")
+
+def get_type(str_a):
+
+  if file_path_re.match(str_a):
+    return 'file_path'
+
+  elif date_re.match(str_a):
+    return 'date'
+
+  elif phone_number_re.match(str_a):
+    return 'phone_number'
+
+  elif ip_address_re.match(str_a):
+    return 'ip_address'
+
+  elif money_re.match(str_a):
+    return 'money'
+
+  elif num_re.match(str_a):
+    return 'number'
+
+  elif version_re.match(str_a):
+    return 'version'
+
+  return 'alphanum'
+
+def get_cmp_file_path(str):
+  file_parts = str.split('/')
+  return (len(file_parts),) + tuple(file_parts)
+
+def get_cmp_date(str):
+  return (datetime.datetime.strptime(str, '%Y-%m-%d') -
+          datetime.datetime.utcfromtimestamp(0)).total_seconds()
+
+def get_cmp_phone_number(str):
+  return phone_number_re.match(str).groups()
+
+def get_cmp_ipaddress(str):
+  return tuple([int(x) for x in str.split('.')])
+
+def get_cmp_money(str):
+  return float(str.replace('$', ''))
+
+def get_cmp_number(str):
+  return float(str)
+
+def get_cmp_version(str):
+  return tuple([int(x) for x in str.split('.')])
+
+def get_cmp_alphanum(str):
+  convert = lambda s: float(s) if num_re.match(s) else s
+  return tuple([convert(c) for c in re.split('([0-9]+)', str.lower())])
+
+type_comp_value_func_mapping = {
+  'file_path': get_cmp_file_path,
+  'date': get_cmp_date,
+  'phone_number': get_cmp_phone_number,
+  'ip_address': get_cmp_ipaddress,
+  'money': get_cmp_money,
+  'number': get_cmp_number,
+  'version': get_cmp_version,
+  'alphanum': get_cmp_alphanum
+}
+
+def get_compare_value_getter(type):
+  return type_comp_value_func_mapping[type]
